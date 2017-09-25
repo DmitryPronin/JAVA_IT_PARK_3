927eb9797f14d4bea01848d98646c384725d263d
student13 <student13@itpark.ru>
2017-09-25 18:58:11 +0300

    task 2-3

---
+++ Tasks/Task2/Program2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Program {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int number = 13745;
+
+		int x5000 = number / 5000;
+		number = number % 5000;
+		int x1000 = number / 1000;
+		number = number % 1000;
+		
+		
+		
+		System.out.println(x7 + "" 
+			+ x6 + "" 
+			+ x5 + "" 
+			+ x4 + ""
+			+ x3 + ""
+			+ x2 + ""
+			+ x1 + ""
+			+ x0);
+
+	}
+}
